6c16826cb5f7bdf2b2ea4511c981b3240cd6fec9
Gmaxtech2024 <gwamakawatson2024@gmail.com>
2025-01-20 03:46:27 +0300

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -1,107 +1,26 @@
-function hi() {
-  console.log("Hello World!");
-}
-hi();
-function hi() {
-  console.log("Hello World!");
-}
-hi();
-const {
-  cmd,
-  commands
-} = require("../command");
-const yts = require("yt-search");
-const axios = require("axios");
-cmd({
-  'pattern': "video2",
-  'alias': ["ytvid2", "ytv2", "ytvideo2"],
-  'react': '⏳',
-  'desc': "Download videos from YouTube by searching for keywords.",
-  'category': "video",
-  'use': ".vidx <keywords>",
-  'filename': __filename
-}, async (_0xe2f6b0, _0xc0bd08, _0x122f70, {
-  from: _0x4938ec,
-  args: _0x545fb3,
-  reply: _0x290209
-}) => {
-  try {
-    const _0x32deeb = _0x545fb3.join(" ");
-    if (!_0x32deeb) {
-      return _0x290209("*Please provide a video tital or url*");
-    }
-    _0x290209("> © ɢᴍᴀx  ɢᴇɴᴇʀᴀᴛɪɴɢ ᴠɪᴅᴇᴏ ᴘʟᴇᴀsᴇ ᴘᴀɪᴛ...");
-    const _0x5e6ce9 = await yts(_0x32deeb);
-    if (!_0x5e6ce9.videos || _0x5e6ce9.videos.length === 0x0) {
-      return _0x290209("❌ No results found for \"" + _0x32deeb + "\".");
-    }
-    const _0x8cec1e = _0x5e6ce9.videos[0x0];
-    const _0x5ce282 = _0x8cec1e.url;
-    const _0x2af2d2 = "https://api.giftedtech.web.id/api/download/dlmp4?apikey=gifted&url=" + _0x5ce282;
-    const _0x442320 = await axios.get(_0x2af2d2);
-    if (!_0x442320.data.success) {
-      return _0x290209("❌ Failed to fetch video for \"" + _0x32deeb + "\".");
-    }
-    const {
-      download_url: _0x503033
-    } = _0x442320.data.result;
-    await _0xe2f6b0.sendMessage(_0x4938ec, {
-      'video': {
-        'url': _0x503033
-      },
-      'mimetype': "video/mp4"
-    }, {
-      'quoted': _0xc0bd08
-    });
-  } catch (_0x3da04a) {
-    console.error(_0x3da04a);
-    _0x290209("❌ An error occurred while processing your request.");
-  }
-});
-cmd({
-  'pattern': "play2",
-  'alias': ["yta2", "ytplay2"],
-  'react': '⏳',
-  'desc': "Download audio from YouTube by searching for keywords.",
-  'category': "music",
-  'use': ".playx <keywords>",
-  'filename': __filename
-}, async (_0x1014d1, _0x44f57d, _0x281123, {
-  from: _0x22ebe3,
-  args: _0x445711,
-  reply: _0x377273
-}) => {
-  try {
-    const _0x356223 = _0x445711.join(" ");
-    if (!_0x356223) {
-      return _0x377273("*Please provide a audio title or url*");
-    }
-    _0x377273(">© ɢᴍᴀx  ɢᴇɴᴇʀᴀᴛɪɴɢ sᴏɴɢ2 ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...");
-    const _0x271698 = await yts(_0x356223);
-    if (!_0x271698.videos || _0x271698.videos.length === 0x0) {
-      return _0x377273("❌ No results found for \"" + _0x356223 + "\".");
-    }
-    const _0x4360a6 = _0x271698.videos[0x0];
-    const _0x57ffbc = _0x4360a6.url;
-    const _0x3af9d4 = "https://api.giftedtech.web.id/api/download/dlmp3?apikey=gifted&url=" + _0x57ffbc;
-    const _0x1d283c = await axios.get(_0x3af9d4);
-    if (!_0x1d283c.data.success) {
-      return _0x377273("❌ Failed to fetch audio for \"" + _0x356223 + "\".");
-    }
-    const {
-      download_url: _0x3a6e3b
-    } = _0x1d283c.data.result;
-    await _0x1014d1.sendMessage(_0x22ebe3, {
-      'audio': {
-        'url': _0x3a6e3b
-      },
-      'mimetype': "audio/mp4",
-      'ptt': false
-    }, {
-      'quoted': _0x44f57d
-    });
-  } catch (_0x274411) {
-    console.error(_0x274411);
-    _0x377273("❌ An error occurred while processing your request.");
-  }
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const _0x3975c9=_0x15ba;(function(_0x28f90a,_0x1fe21e){const _0x1043e9=_0x15ba,_0x5e2ee2=_0x28f90a();while(!![]){try{const _0x2ff00e=-parseInt(_0x1043e9(0x159))/0x1*(parseInt(_0x1043e9(0x13d))/0x2)+parseInt(_0x1043e9(0x13a))/0x3*(-parseInt(_0x1043e9(0x141))/0x4)+parseInt(_0x1043e9(0x147))/0x5*(parseInt(_0x1043e9(0x133))/0x6)+-parseInt(_0x1043e9(0x153))/0x7*(-parseInt(_0x1043e9(0x14e))/0x8)+-parseInt(_0x1043e9(0x12f))/0x9+-parseInt(_0x1043e9(0x158))/0xa+parseInt(_0x1043e9(0x131))/0xb;if(_0x2ff00e===_0x1fe21e)break;else _0x5e2ee2['push'](_0x5e2ee2['shift']());}catch(_0x5d0812){_0x5e2ee2['push'](_0x5e2ee2['shift']());}}}(_0x116c,0x92b6c));function hi(){const _0x3a6c80=_0x15ba;console[_0x3a6c80(0x15a)](_0x3a6c80(0x154));}function _0x116c(){const _0x12d8c4=['https://api.giftedtech.web.id/api/download/dlmp3?apikey=gifted&url=','19829909zDVrfX','ytplay2','102rwRaRi','length','ytv2','music','videos','join','>\x20©\x20ɢᴍᴀx\x20\x20ɢᴇɴᴇʀᴀᴛɪɴɢ\x20ᴠɪᴅᴇᴏ\x20ᴘʟᴇᴀsᴇ\x20ᴘᴀɪᴛ...','57anhhsq','video2','Download\x20audio\x20from\x20YouTube\x20by\x20searching\x20for\x20keywords.','748486QDeRih','*Please\x20provide\x20a\x20video\x20tital\x20or\x20url*','❌\x20No\x20results\x20found\x20for\x20\x22','success','137796wxrYPF','play2','https://api.giftedtech.web.id/api/download/dlmp4?apikey=gifted&url=','.playx\x20<keywords>','ytvideo2','Download\x20videos\x20from\x20YouTube\x20by\x20searching\x20for\x20keywords.','48110kfaIkx','sendMessage','audio/mp4','video/mp4','.vidx\x20<keywords>','yt-search','data','931064xwsyBi','*Please\x20provide\x20a\x20audio\x20title\x20or\x20url*','❌\x20Failed\x20to\x20fetch\x20audio\x20for\x20\x22','url','video','49nJbQho','Hello\x20World!','error','❌\x20An\x20error\x20occurred\x20while\x20processing\x20your\x20request.','result','8358810EpjJmT','1vrmgkh','log','yta2','2838411tkEhwG'];_0x116c=function(){return _0x12d8c4;};return _0x116c();}hi();function hi(){console['log']('Hello\x20World!');}hi();const {cmd,commands}=require('../command'),yts=require(_0x3975c9(0x14c)),axios=require('axios');function _0x15ba(_0x2e1c60,_0x3aa6ea){const _0x116c0d=_0x116c();return _0x15ba=function(_0x15babb,_0x3e2a05){_0x15babb=_0x15babb-0x12f;let _0x32fede=_0x116c0d[_0x15babb];return _0x32fede;},_0x15ba(_0x2e1c60,_0x3aa6ea);}cmd({'pattern':_0x3975c9(0x13b),'alias':['ytvid2',_0x3975c9(0x135),_0x3975c9(0x145)],'react':'⏳','desc':_0x3975c9(0x146),'category':_0x3975c9(0x152),'use':_0x3975c9(0x14b),'filename':__filename},async(_0x20f650,_0x765204,_0x582db9,{from:_0x3cd1f8,args:_0x89abb7,reply:_0x5c0791})=>{const _0x41f23e=_0x3975c9;try{const _0x1e983a=_0x89abb7[_0x41f23e(0x138)]('\x20');if(!_0x1e983a)return _0x5c0791(_0x41f23e(0x13e));_0x5c0791(_0x41f23e(0x139));const _0x30ed01=await yts(_0x1e983a);if(!_0x30ed01[_0x41f23e(0x137)]||_0x30ed01['videos']['length']===0x0)return _0x5c0791('❌\x20No\x20results\x20found\x20for\x20\x22'+_0x1e983a+'\x22.');const _0x1c566d=_0x30ed01[_0x41f23e(0x137)][0x0],_0x486d4b=_0x1c566d['url'],_0x156d5d=_0x41f23e(0x143)+_0x486d4b,_0x545907=await axios['get'](_0x156d5d);if(!_0x545907[_0x41f23e(0x14d)][_0x41f23e(0x140)])return _0x5c0791('❌\x20Failed\x20to\x20fetch\x20video\x20for\x20\x22'+_0x1e983a+'\x22.');const {download_url:_0x51f6e3}=_0x545907[_0x41f23e(0x14d)][_0x41f23e(0x157)];await _0x20f650[_0x41f23e(0x148)](_0x3cd1f8,{'video':{'url':_0x51f6e3},'mimetype':_0x41f23e(0x14a)},{'quoted':_0x765204});}catch(_0x11987f){console[_0x41f23e(0x155)](_0x11987f),_0x5c0791(_0x41f23e(0x156));}}),cmd({'pattern':_0x3975c9(0x142),'alias':[_0x3975c9(0x15b),_0x3975c9(0x132)],'react':'⏳','desc':_0x3975c9(0x13c),'category':_0x3975c9(0x136),'use':_0x3975c9(0x144),'filename':__filename},async(_0x48b5e3,_0x3e1065,_0x1067a6,{from:_0x20f9c3,args:_0x47bd9e,reply:_0x18ffb4})=>{const _0x5dc178=_0x3975c9;try{const _0x4bc597=_0x47bd9e[_0x5dc178(0x138)]('\x20');if(!_0x4bc597)return _0x18ffb4(_0x5dc178(0x14f));_0x18ffb4('>©\x20ɢᴍᴀx\x20\x20ɢᴇɴᴇʀᴀᴛɪɴɢ\x20sᴏɴɢ2\x20ᴘʟᴇᴀsᴇ\x20ᴡᴀɪᴛ...');const _0x33396e=await yts(_0x4bc597);if(!_0x33396e[_0x5dc178(0x137)]||_0x33396e[_0x5dc178(0x137)][_0x5dc178(0x134)]===0x0)return _0x18ffb4(_0x5dc178(0x13f)+_0x4bc597+'\x22.');const _0x5077c0=_0x33396e[_0x5dc178(0x137)][0x0],_0x2bc160=_0x5077c0[_0x5dc178(0x151)],_0x55ee89=_0x5dc178(0x130)+_0x2bc160,_0x54aeda=await axios['get'](_0x55ee89);if(!_0x54aeda[_0x5dc178(0x14d)][_0x5dc178(0x140)])return _0x18ffb4(_0x5dc178(0x150)+_0x4bc597+'\x22.');const {download_url:_0x2a3697}=_0x54aeda[_0x5dc178(0x14d)]['result'];await _0x48b5e3[_0x5dc178(0x148)](_0x20f9c3,{'audio':{'url':_0x2a3697},'mimetype':_0x5dc178(0x149),'ptt':![]},{'quoted':_0x3e1065});}catch(_0x46dcd7){console[_0x5dc178(0x155)](_0x46dcd7),_0x18ffb4(_0x5dc178(0x156));}});
