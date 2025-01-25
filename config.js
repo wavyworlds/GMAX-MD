43aba4eeb0ae654eaf8eae1d7d56d81573aab2f1
Gmaxtech2024 <gwamakawatson2024@gmail.com>
2025-01-25 20:54:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,86 +5,61 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your gmax md session id here",
- 
-PREFIX: process.env.PREFIX || ".",
-// Enter Your Desired Prefix
-
-BOT_NAME: process.env.BOT_NAME || "『 𝐆𝐌𝐀𝐗 𝐌𝐃 』",
-// Enter Your Bot Name
-
-ANTI_VV: process.env.ANTI_VV || "true",
-// Allow Viewoncs view Unlimited 
- 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-// make this true for custum emoji react  
-  
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
-// chose custom react emojis by yourself 
-
-DELETE_LINKS: process.env.DELETE_LINKS || "true",
-// automatic delete links witho remove member 
-
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255622053093",
-// Set Owner Name
-
-OWNER_NAME: process.env.OWNER_NAME || "sɪʀ ɢᴍᴀx",
-// Set Footer
-
-DESCRIPTION: process.env.DESCRIPTION || "*© Gᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ɢᴍᴀx*",
-// add bot owner name   
- 
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/6BMJmGY/mrfrankofc.jpg",
-// add img for alive msg
-
-LIVE_MSG: process.env.LIVE_MSG || "> [🎐] ɢᴍᴀx MD ɪs ᴏɴʟɪɴᴇ*⚡",
-// add alive msg here 
-
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
-// Turn true or false for automatic read msgs
-
-AUTO_REACT: process.env.AUTO_REACT || "false",
-// make this true or false for auto react on all msgs
-
-ANTI_BAD: process.env.ANTI_BAD || "true",
-// false or true for anti bad words
-
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
+SESSION_ID: process.env.SESSION_ID || "",
+// add your Session Id 
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
-
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
-
-AUTO_STATUS_MSG: process.env.AUTO_STATUS__MSG || "*[❄️] Hi there, GMAX-MD viewed your Status🎐*",
-
-// set the auto reply massage on status reply    
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY GMAX🤍*",
+// set the auto reply massage on status reply  
+PREFIX: process.env.PREFIX || ".",
+// add your prifix for bot   
+BOT_NAME: process.env.BOT_NAME || "GMAX-MD",
+// add bot namw here for menu
+STICKER_NAME: process.env.STICKER_NAME || "GMAX-MD",
+// type sticker pack name 
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+// make this true for custum emoji react    
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+// chose custom react emojis by yourself 
+DELETE_LINKS: process.env.DELETE_LINKS || "false",
+// automatic delete links witho remove member 
+OWNER_NUMBER: process.env.OWNER_NUMBER || "255753853473",
+// add your bot owner number
+OWNER_NAME: process.env.OWNER_NAME || "Mr Gmax",
+// add bot owner name
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ɢᴍᴀx*",
+// add bot owner name    
+ALIVE_IMG: process.env.ALIVE_IMG || "",
+// add img for alive msg
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
+AUTO_REACT: process.env.AUTO_REACT || "false",
+// make this true or false for auto react on all msgs
+ANTI_BAD: process.env.ANTI_BAD || "false",
+// false or true for anti bad words  
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
-
 ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
-
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
-
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
-
-AUTO_REPLY: process.env.AUTO_REPLY || "true",
-// make true or false automatic text reply
- 
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+// make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
-
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
-
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
-// true for automatic show typing  
- 
-READ_CMD: process.env.READ_CMD || "true",
-// true if want mark commands as read 
-
+// true for automatic show typing   
+DEV: process.env.DEV || "255753853473",
+//replace with your whatsapp number        
+ANTI_VV: process.env.ANTI_VV || "true",
+// true for anti once view 
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", // change it to 'same' if you want to resend deleted message in same chat 
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
 // make it true for auto recoding 
 };
