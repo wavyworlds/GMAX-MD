ed25eefa5d6e71006430341d0fa46e088269fc5a
Gmaxtech2024 <gwamakawatson2024@gmail.com>
2025-02-03 09:57:23 +0300

    Update gemin.js

--- plugins/gemin.js
+++ plugins/gemin.js
@@ -1,5 +1,6 @@
 
  const axios = require("axios")
+ const { cmd } = require("../command");
 
  const GEMINI_API_KEY = "";   //REPLACE WITH YOUR API KEY OF GEMINI
  const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
